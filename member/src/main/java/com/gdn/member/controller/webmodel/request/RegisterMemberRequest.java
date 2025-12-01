04f4f9bd25b127a3bacecfbc55cf4c0d17ced6fe
GDN\handi.lieputra <handi.lieputra@gdn-commerce.com>
2025-12-01 17:44:18 +0700

    Add more validation for register member request Data

--- member/src/main/java/com/gdn/member/controller/webmodel/request/RegisterMemberRequest.java
+++ member/src/main/java/com/gdn/member/controller/webmodel/request/RegisterMemberRequest.java
@@ -1,6 +1,8 @@
 package com.gdn.member.controller.webmodel.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,13 +11,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterMemberRequest {
-  @NotBlank
+  @NotBlank(message = "Username is required")
+  @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters")
+  @Pattern(regexp = "^[a-zA-Z0-9._-]+$", message = "Username can only contain letters, numbers, ., _, -")
   private String username;
-  @NotBlank
+  @NotBlank(message = "Password is required")
+  @Size(min = 8, max = 50, message = "Password must be at least 8 characters")
+  @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=]).*$",
+    message = "Password must contain letters, numbers, and special characters")
   private String password;
-  @NotBlank
+  @NotBlank(message = "Name is required")
   private String name;
-  @NotBlank
+  @NotBlank(message = "Address is required")
+  @Size(max = 200, message = "Address cannot exceed 200 characters")
   private String address;
 
 }
